7973fc86b8bcc6eb1cfb80b1df7de3378516de94
Joseph Angelo Perez <79298332+jsphprz@users.noreply.github.com>
2021-06-02 21:42:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 #Get your token here: https://developer.spotify.com/console/get-current-user/
 #Insert your Spotify token here:
-x = SpotifyRecom("BQAF7lP7OHWO9r_IapnI6MjTm4Ga5BM9yk9a19iPtZuvpAEXAcHYJI4ScWpivR6aIYyIRGbUOsqHWZoj40jpBPmTsT_YIuZ9LAntNuCV9YqbADg-PR_WLu686mkRQS0H8IjSeqwZCV2n8rV-OB-pA2HXxKd0dPGMWT6oR6It-g0mK1iV1Ca9pEBL9a6SihzZe9kHfW2S9_Fs_zSzDNghrM66WswiqkmEZJZUK4JopTO3qSfbuN0C8CK5pUI3I1M2x0RlJs3pEVLR5fthHn9_CQ")
+x = SpotifyRecom("TOKEN")
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
